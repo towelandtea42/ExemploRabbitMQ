3a10ad859d54fd8a52b3355ee9726f3517f3b0aa
Ubuntu <ubuntu@ip-172-31-80-52.ec2.internal>
2019-12-03 19:22:39 +0000

    Mudanças feitas durante a aula

--- src/main/java/br/ufs/dcomp/ExemploRabbitMQ/Emissor.java
+++ src/main/java/br/ufs/dcomp/ExemploRabbitMQ/Emissor.java
@@ -10,7 +10,7 @@ public class Emissor {
 
   public static void main(String[] argv) throws Exception {
     ConnectionFactory factory = new ConnectionFactory();
-    factory.setUri("amqp://...");
+    factory.setUri("amqp://iohvniqb:6qgJACu1uFgxO2xMV7RrGX8dw2NhnfDg@zebra.rmq.cloudamqp.com/iohvniqb");
     Connection connection = factory.newConnection();
     Channel channel = connection.createChannel();
 
